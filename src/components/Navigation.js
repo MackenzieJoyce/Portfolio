ee6ba8a2234224761d03e09a02fe44c785a45474
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-21 23:39:01 -0400

    add icons on menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,25 @@
 import { Link } from 'react-router-dom';
+import { FaUser, FaCode, FaPhone, FaFileAlt } from 'react-icons/fa';
 
 const Navigation = () => {
   return (
     <header>
       <nav>
         <h3>
+          <FaUser />
+          {/* FaGrinBeam */}
           <Link to="/">About</Link>
         </h3>
         <h3>
+          <FaCode />
           <Link to="/portfolio">Portfolio</Link>
         </h3>
         <h3>
+          <FaPhone />
           <Link to="/contact">Contact</Link>
         </h3>
         <h3>
+          <FaFileAlt />
           <Link to="/resume">Resume</Link>
         </h3>
       </nav>
