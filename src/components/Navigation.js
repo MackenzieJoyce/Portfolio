4bd8a6cdd42d748b4de2b3ae7e9407dcca2f4d08
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-21 20:41:52 -0400

    add Navigation component

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+const Navigation = ({ }) => { 
+    return ( 
+        <header>
+            <nav>
+                <h3>About</h3>
+                <h3>Portfolio</h3>
+                <h3>Contact</h3>
+                <h3>Resume</h3>
+            </nav>
+        </header>
+    )
+}
+
+export default Navigation 
