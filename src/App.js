a0d4fc4cd6a91ee41f64e523cbbb766c65195037
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-21 22:51:53 -0400

    add resume component. Has a jumbotron and accordianto put side by side

--- src/App.js
+++ src/App.js
@@ -4,72 +4,56 @@ import Navigation from './components/Navigation';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
+import Resume from './components/Resume';
 
 function App() {
-
   const [project, setProject] = useState([
     {
       id: 1,
       title: 'Backyard Jungle',
       text: 'An online forum for Gardeners',
-      tech: 'Handlebars/Bootstrap/Express/RESTful API',
+      tech: 'Handlebars/Bootstrap/Express/RESTful API'
     },
     {
       id: 2,
       title: 'Mix Up Movie Night',
       text: 'Your great night chosen by our app',
-      tech: 'HTML/CSS/JavaScript',
+      tech: 'HTML/CSS/JavaScript'
     },
     {
       id: 3,
       title: 'Note Taker',
       text: 'Simple design for all your complicated tasks',
-      tech: 'React',
+      tech: 'React'
     },
     {
       id: 4,
       title: 'Conversation Ender',
-      text: 'Your choices matter, don\'t let the conversation die',
-      tech: 'HTML/CSS/JavaScript',
+      text: "Your choices matter, don't let the conversation die",
+      tech: 'HTML/CSS/JavaScript'
     },
     {
       id: 5,
       title: 'E-commerce Back End',
       text: 'Set up your next e-commerce site',
-      tech: 'Express',
+      tech: 'Express'
     },
     {
       id: 6,
       title: 'Text Editor Extreme',
       text: 'Install the app and take your notes on the go',
-      tech: 'Back-End JavaScript',
-    },
+      tech: 'Back-End JavaScript'
+    }
   ]);
 
   return (
     <div className="container">
       <Navigation />
       <Routes>
-        <Route
-          path="/"
-          element={
-              <About />
-          }
-        />
-        <Route 
-          path='/portfolio'
-          element={
-              <Portfolio
-                project={project}
-              />
-          }
-        />
-        <Route 
-          path='/contact'
-          element={
-              <Contact />
-          }
-        />
+        <Route path="/" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio project={project} />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
       </Routes>
     </div>
   );
---
+++ src/components/Resume.js
@@ -0,0 +1,96 @@
+// I think id needs to be useId 
+
+const Resume = () => {
+  return (
+    <div>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">Work History</h1>
+          <p className="lead">
+            I have never done anything except web development. 
+          </p>
+        </div>
+      </div>
+      <div id="accordion">
+        <div className="card">
+          <div className="card-header" id="headingOne">
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link"
+                data-toggle="collapse"
+                data-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+                Projects
+              </button>
+            </h5>
+          </div>
+
+          <div
+            id="collapseOne"
+            className="collapse show"
+            aria-labelledby="headingOne"
+            data-parent="#accordion"
+          >
+            <div className="card-body">
+              Add the projects in a list and put the text in here rather than the portfolio. 
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-header" id="headingTwo">
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link collapsed"
+                data-toggle="collapse"
+                data-target="#collapseTwo"
+                aria-expanded="false"
+                aria-controls="collapseTwo"
+              >
+                Work Experience
+              </button>
+            </h5>
+          </div>
+          <div
+            id="collapseTwo"
+            className="collapse"
+            aria-labelledby="headingTwo"
+            data-parent="#accordion"
+          >
+            <div className="card-body">
+              List out actual work expereince without mentioning the years 
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-header" id="headingThree">
+            <h5 className="mb-0">
+              <button
+                className="btn btn-link collapsed"
+                data-toggle="collapse"
+                data-target="#collapseThree"
+                aria-expanded="false"
+                aria-controls="collapseThree"
+              >
+                Education and Certifications 
+              </button>
+            </h5>
+          </div>
+          <div
+            id="collapseThree"
+            className="collapse"
+            aria-labelledby="headingThree"
+            data-parent="#accordion"
+          >
+            <div className="card-body">
+              Bootcamp certificate and then regular education without mentioning the degree or year 
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Resume;
