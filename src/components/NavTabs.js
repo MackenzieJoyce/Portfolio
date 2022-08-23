bb6ce04cd5683357a1d8883f7563c0530fd5c9e8
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-23 18:08:04 -0400

    added bottom margin

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import { FaUser, FaCode, FaPhone, FaFileAlt } from 'react-icons/fa';
@@ -10,7 +11,13 @@ import Resume from './pages/Resume';
 
 import project from '../project';
 
-const NavTabs = () => {
+const styles = {
+  nav: {
+    marginBottom: 30
+  }
+};
+
+const App = () => {
   // Says which tab is currently clicked
   const [selectedTab, setSelectedTab] = React.useState(0);
 
@@ -20,34 +27,34 @@ const NavTabs = () => {
   };
 
   return (
-      <>
-        <Tabs
-          value={selectedTab}
-          onChange={handleChange}
-          aria-label="icon tabs for navigation"
+    <header>
+      <Tabs
+        value={selectedTab}
+        onChange={handleChange}
+        aria-label="icon tabs for navigation"
+        style={styles.nav}
         centered
-        // Supposed to make the underline lightBlue 
-        // TextIndicatorProps={{sx: { backgroundColor: 'lightBlue', height: 4}}} 
-        
-        sx={{ 
-          "button": { color: '#fff' },
-          "button:hover": {color: 'gray'},
-          "button.Mui-selected": {color: 'lightBlue'}
+        // Supposed to make the underline lightBlue
+        // TextIndicatorProps={{sx: { backgroundColor: 'lightBlue', height: 4}}}
+        sx={{
+          button: { color: '#fff' },
+          'button:hover': { color: 'gray' },
+          'button.Mui-selected': { color: 'lightBlue' }
         }}
-        >
-          <Tab icon={<FaUser />} label="ABOUT" />
-          <Tab icon={<FaCode />} label="PORTFOLIO" />
-          <Tab icon={<FaPhone />} label="CONTACT" />
-          <Tab icon={<FaFileAlt />} label="RESUME" />
-        </Tabs>
-        {/* Says: When the tab selected is at # it will show the specific page info */}
-        {/* This is sorta like a turnary but not */}
-        {selectedTab === 0 && <About />}
-        {selectedTab === 1 && <Portfolio project={project} />}
-        {selectedTab === 2 && <Contact />}
-        {selectedTab === 3 && <Resume />}
-      </>
+      >
+        <Tab icon={<FaUser />} label="ABOUT" />
+        <Tab icon={<FaCode />} label="PORTFOLIO" />
+        <Tab icon={<FaPhone />} label="CONTACT" />
+        <Tab icon={<FaFileAlt />} label="RESUME" />
+      </Tabs>
+      {/* Says: When the tab selected is at # it will show the specific page info */}
+      {/* This is sorta like a turnary but not */}
+      {selectedTab === 0 && <About />}
+      {selectedTab === 1 && <Portfolio project={project} />}
+      {selectedTab === 2 && <Contact />}
+      {selectedTab === 3 && <Resume />}
+    </header>
   );
 };
 
-export default NavTabs;
+export default App;
