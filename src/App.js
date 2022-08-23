2746970b21da5b9420b6b9d9a50ec4320558409a
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-23 18:57:24 -0400

    added styling to about page. commented out browser router in app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import PortfolioContainer from './components/PortfolioContainer';
+// import { BrowserRouter } from 'react-router-dom';
 
-const App = () => <PortfolioContainer />;
+const App = () => {
+  return (
+      <PortfolioContainer />
+  );
+};
 
+// Make obj and set up browserrouter around ^
 export default App;
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,16 +1,30 @@
 import React from 'react';
 
+const styles = {
+    contentEntire: {
+        textAlign: 'center',
+        border: '2px solid lightblue',
+        borderRadius: 5,
+        background: 'grey',
+        minHeight: 500,
+        margin: '0 30px',
+        width: '80%'
+    },
+    contentIndiv: { 
+        width: '30%'
+    }
+}
 
 const About = () => { 
     return ( 
-        <div className="content">
-            <div>
+        <div className="content" style={styles.contentEntire}>
+            <div style={styles.contentIndiv}>
                 {/* <img></img> */}
                 <h1>
                     Mackenzie Joyce
                 </h1>
             </div>
-            <div>
+            <div style={styles.contentIndiv}>
                 <h2>
                     Front-End Web Developer
                 </h2>
