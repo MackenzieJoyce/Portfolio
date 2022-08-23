d188373000ab79636aec9a4e2b2044725891bf14
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-22 22:31:15 -0400

    attempting to override the styling brought in by mui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import { FaUser, FaCode, FaPhone, FaFileAlt } from 'react-icons/fa';
@@ -13,6 +14,19 @@ import Resume from './components/Resume';
 import project from './project';
 
 const App = () => {
+  const theme = createTheme({
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            backgroundColor: 'rgb(12, 12, 12)',
+            color: '#fff'
+          }
+        }
+      }
+    }
+  });
+
   // Says which tab is currently clicked
   const [selectedTab, setSelectedTab] = React.useState(0);
 
@@ -22,25 +36,27 @@ const App = () => {
   };
 
   return (
-    <header>
-      <Tabs
-        value={selectedTab}
-        onChange={handleChange}
-        aria-label="icon tabs for navigation" 
-        centered
-      >
-        <Tab icon={<FaUser />} label="ABOUT" />
-        <Tab icon={<FaCode />} label="PORTFOLIO" />
-        <Tab icon={<FaPhone />} label="CONTACT" />
-        <Tab icon={<FaFileAlt />} label="RESUME" />
-      </Tabs>
-      {/* Says: When the tab selected is at # it will show the specific page info */}
-      {/* This is sorta like a turnary but not */}
-      {selectedTab === 0 && <About />}
-      {selectedTab === 1 && <Portfolio project={project} />}
-      {selectedTab === 2 && <Contact />}
-      {selectedTab === 3 && <Resume />}
-    </header>
+    <ThemeProvider theme={theme}>
+      <header>
+        <Tabs
+          value={selectedTab}
+          onChange={handleChange}
+          aria-label="icon tabs for navigation"
+          centered
+        >
+          <Tab icon={<FaUser />} label="ABOUT" />
+          <Tab icon={<FaCode />} label="PORTFOLIO" />
+          <Tab icon={<FaPhone />} label="CONTACT" />
+          <Tab icon={<FaFileAlt />} label="RESUME" />
+        </Tabs>
+        {/* Says: When the tab selected is at # it will show the specific page info */}
+        {/* This is sorta like a turnary but not */}
+        {selectedTab === 0 && <About />}
+        {selectedTab === 1 && <Portfolio project={project} />}
+        {selectedTab === 2 && <Contact />}
+        {selectedTab === 3 && <Resume />}
+      </header>
+    </ThemeProvider>
   );
 };
 
