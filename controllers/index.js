6ba996fdd52dd5642410404bef066997e803f6c7
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-23 16:39:32 -0400

    edit oopsy

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
 const emailRoutes = require('./emailRoutes');
 
-router.use('/contact, emailRoutes');
+router.use('/contact', emailRoutes);
+
+module.exports = router;
