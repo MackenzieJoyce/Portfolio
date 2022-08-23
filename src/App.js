b531558b0f0fdd7cfad68b6bee81e446eb777ae0
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-22 21:24:24 -0400

    removed the navigation component and moved all that content to replace the app.js content. The Navigation has all the tabs for the pages and imports that are being used so it just didn't need to be it's on module

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import { FaUser, FaCode, FaPhone, FaFileAlt } from 'react-icons/fa';
 
-import Navigation from './components/Navigation';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
 
-function App() {
+import project from './project';
 
+
+const App = () => {
+  // Says which tab is currently clicked
+  const [selectedTab, setSelectedTab] = React.useState(0);
+
+  // Fxn triggered whenever you click another tab
+  const handleChange = (event, newValue) => {
+    setSelectedTab(newValue);
+  };
 
   return (
-    <div className="container">
-      <Navigation />
-      <Router>
-        <Routes>
-          {/* <Route path="/" element={<About />} />
-          <Route path="portfolio" element={<Portfolio project={project} />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="resume" element={<Resume />} />
-          If they try anything else it all explodes 
-          <Route
-            path="*"
-            element={
-              <main style={{ padding: '1rem' }}>
-                <p>there's nothing here!</p>
-              </main>
-            }
-          /> */}
-        </Routes>
-      </Router>
-    </div>
+    <header>
+      <Tabs
+        value={selectedTab}
+        onChange={handleChange}
+        aria-label="icon tabs for navigation"
+      >
+        <Tab icon={<FaUser />} label="ABOUT" />
+        <Tab icon={<FaCode />} label="PORTFOLIO" />
+        <Tab icon={<FaPhone />} label="CONTACT" />
+        <Tab icon={<FaFileAlt />} label="RESUME" />
+      </Tabs>
+      {/* Says: When the tab selected is at # it will show the specific page info */}
+      {/* This is sorta like a turnary but not */}
+      {selectedTab === 0 && <About />}
+      {selectedTab === 1 && <Portfolio project={project} />}
+      {selectedTab === 2 && <Contact />}
+      {selectedTab === 3 && <Resume />}
+    </header>
   );
-}
+};
 
-export default App;
+export default App
