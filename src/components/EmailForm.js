3a7b4c8f794fc2824f2874239fd757c8200567da
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-23 10:02:27 -0400

    add component for the email form in the contact page

---
+++ src/components/EmailForm.js
@@ -0,0 +1,62 @@
+import { RemoveShoppingCartRounded } from '@mui/icons-material';
+import React, { useState } from 'react';
+
+const EmailForm = () => {
+  const [status, setStatus] = useState('Submit');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setStatus('Sending now...');
+
+    const { name, email, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value
+    };
+    let response = await fetch('http://localhost:5000/contact', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'http://localhost:5000/contact'
+      },
+      body: JSON.stringify(details)
+    });
+
+    setStatus('Submit');
+    let result = await RemoveShoppingCartRounded.json();
+    alert(result.status);
+    // setEmail('');
+    // setMessage('');
+  };
+
+  return (
+    <div className="container">
+      <h1>Get In Touch!</h1>
+      <form className="add-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">Name </label>
+          <input type="text" id="name" placeholder="Your Name" required />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="email">Email </label>
+          <input type="email" id="email" placeholder="Your Email" required />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="message">Message </label>
+          <input
+            type="textarea"
+            id="message"
+            placeholder="Your Message"
+            required
+          />
+        </div>
+        <button type="submit">{status}</button>
+      </form>
+    </div>
+  );
+};
+
+export default EmailForm;
