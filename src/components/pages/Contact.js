6846b090538de216dd8c16ebe0616899e8280d18
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-24 18:17:46 -0400

    added linked in, fixed the _blank issue, some styling

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { FaGithub, FaTwitterSquare, FaDiscord } from 'react-icons/fa';
+import {
+  FaGithub,
+  FaTwitterSquare,
+  FaDiscord,
+  FaLinkedin
+} from 'react-icons/fa';
 
 import EmailForm from './compPlugins/EmailForm';
 
 const styles = {
-  title: {
-    textAlign: 'center',
-    marginBottom: 30
-  },
   list: {
     fontSize: '1.5rem',
     display: 'flex',
-    flexDirection: 'column'
+    flexDirection: 'column',
+    alignItems: 'center'
   },
   span: {
-    marginBottom: 40,
+    marginBottom: '8%',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center'
@@ -24,52 +26,50 @@ const styles = {
 const Contact = () => {
   return (
     <>
-      {/* <h3 style={styles.title}>Get In Touch</h3> */}
-
       <div className="content contentEntire">
-        <div className='contentIndiv'>
-        {/* className="container" */}
+        <div className="contentIndiv">
           <h3 className="title">Get In Touch</h3>
           <div style={styles.list}>
-            <span style={styles.span}>
-              <FaGithub />
-              <a
-                href="https://github.com/MackenzieJoyce"
-                // target="_blank"
-                title="Github: MackenzieJoyce"
-              >
-                MackenzieJoyce
-              </a>
-            </span>
-            <span style={styles.span}>
-              <FaTwitterSquare />
-              <a
-                href="https://twitter.com/FreshCodeFish"
-                // target="_blank"
-                title="Twitter: FreshCodeFish"
-              >
-                FreshCodeFish
-              </a>
-            </span>
+            <a
+              href="https://github.com/MackenzieJoyce"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Github: MackenzieJoyce"
+            >
+              <span style={styles.span}>
+                <FaGithub />
+                GitHub - MackenzieJoyce
+              </span>
+            </a>
+
+            <a
+              href="https://www.linkedin.com/in/mackenziejoyce/"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Github: MackenzieJoyce"
+            >
+              <span style={styles.span}>
+                <FaLinkedin />
+                LinkedIn - MackenzieJoyce
+              </span>
+            </a>
+
+            <a
+              href="https://twitter.com/FreshCodeFish"
+              target="_blank"
+              rel="noopener noreferrer"
+              title="Twitter: FreshCodeFish"
+            >
+              <span style={styles.span}>
+                <FaTwitterSquare />
+                Twitter - FreshCodeFish
+              </span>
+            </a>
+
             <span style={styles.span}>
               <FaDiscord />
-              MackenzieJoyce#5843
+              Discord - MackenzieJoyce#5843
             </span>
-            {/* <span>
-          <FaDice20 />
-          Roll20: RollingUpRoses
-        </span> */}
-            {/* <span>LinkedIn</span>
-        <span>Share this portfolio</span> */}
-            {/* 
-          <span>
-            Phone
-            <a href="tel:321.234.5397" title="Phone number: 321.234.5397">
-              321.234.5397
-            </a>
-          </span> */}
-
-            {/* <span style={styles.span}>Email</span> */}
           </div>
         </div>
         <EmailForm />
@@ -77,9 +77,5 @@ const Contact = () => {
     </>
   );
 };
-
-// target _blank's are giving errors
-
-// Add the EmailForm so you can toggle the option
 
 export default Contact;
