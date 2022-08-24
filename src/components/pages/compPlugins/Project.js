8a3e6abbc4e033df16e9daa274713475c372dfdd
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-24 15:58:15 -0400

    tried making the card content hide and pop up on hover... did not work. moving on

--- src/components/pages/compPlugins/Project.js
+++ src/components/pages/compPlugins/Project.js
@@ -4,9 +4,11 @@ const Project = ({ project }) => {
   return (
     <div className="projectCard">
       <img src={project.img} alt={project.title} />
-      <h3 className="title">{project.title}</h3>
-      <p className="">{project.text}</p>
-      <span className="standout">{project.tech}</span>
+      <div className='projectCardContent'>
+        <h3 className="title">{project.title}</h3>
+        <p className="">{project.text}</p>
+        <span className="standout">{project.tech}</span>
+      </div>
     </div>
   );
 };
