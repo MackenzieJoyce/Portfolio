a4f61655589ed95572c22edacf50e6b8555319a7
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-23 21:43:47 -0400

    added content to about page and some styling to make it cute

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -14,7 +14,10 @@ const About = () => {
                     Front-End Web Developer
                 </h2>
                 <p>
-                    I am now and always have been a front-end web developer and no one can tell me otherwise
+                    I am a recent graduate of a full-stack coding bootcamp. My strong suit is creating web apps that are clean, simple, yet sitll have character and draw people in. 
+                </p>
+                <p>
+                    Moving forward, I will be honing my skills in front-end frameworks and libraries, and continuing my practice in back-end development. I am looking for work, so please feel free to meesage me regarding opportunities... Or just say hi! 
                 </p>
             </div>
         </div>
