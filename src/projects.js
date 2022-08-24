a74d0e8359d66e5da841a61034177d8241f754c7
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-24 14:15:33 -0400

    Not working - gave an extra ../ so that when the value gets put in it SHOULD go to the correct route

--- src/projects.js
+++ src/projects.js
@@ -1,42 +1,42 @@
 const projects = [
   {
     id: 1,
-    img: './img/Backyard Jungle.png',
+    img: '../../img/Backyard Jungle.png',
     title: 'Backyard Jungle',
     text: 'An online forum for Gardeners',
     tech: 'Handlebars/Bootstrap/Express/RESTful API'
   },
   {
     id: 2,
-    img: './img/mixUpMovieNight.png',
+    img: '../../img/mixUpMovieNight.png',
     title: 'Mix Up Movie Night',
     text: 'Your great night chosen by our app',
     tech: 'CSS/Bulma/JavaScript'
   },
   {
     id: 3,
-    img: './img/noteTaker.png',
+    img: '../../img/noteTaker.png',
     title: 'Note Tracker',
     text: 'Simple design for all your complicated tasks',
     tech: 'React'
   },
   {
     id: 4,
-    img: './img/nerdWall.png',
+    img: '../../img/nerdWall.png',
     title: 'Conversation Ender',
     text: "Your choices matter, don't let the conversation die",
     tech: 'CSS/JavaScript'
   },
   {
     id: 5,
-    img: './img/techBlog.png',
+    img: '../../img/techBlog.png',
     title: 'Tech Blog',
     text: 'Make an account, share thoughts, interact with others',
     tech: 'Express'
   },
   {
     id: 6,
-    img: './img/textEditor.png',
+    img: '../../img/textEditor.png',
     title: 'Text Editor Extreme',
     text: 'Install the app and take your notes on the go',
     tech: 'JavaScript'
