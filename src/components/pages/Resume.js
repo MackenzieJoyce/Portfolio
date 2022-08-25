20e65c57f3284e46356d34aee96232e904e88be4
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-25 10:24:45 -0400

    START OF RESTYLING - started styling the resume again... button still not right

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,38 +2,35 @@
 import React from 'react';
 
 const styles = {
-  little: {
-    width: 400
-  },
-  short: {
+  underlineSmaller: {
     margin: '0 15%'
   },
-  miniTitle: {
+  subTitle: {
     margin: '5% 0 5% 0'
   },
-  sides: { 
+  leftAndRight: {
     width: '100%',
     marginBottom: '10%',
     display: 'flex',
-    justifyContent: 'space-around', 
+    justifyContent: 'space-around',
   }
 };
 
 const Resume = () => {
-    const onDownload = () => {
-      const press = document.createElement("a");
-      press.download = `Resume_Mackenzie_Joyce.pdf`;
-      press.href = "../../public/Resume_Mackenzie_Joyce.pdf";
-      press.click();
-    };
+  const onDownload = () => {
+    const press = document.createElement('a');
+    press.download = `Resume_Mackenzie_Joyce.pdf`;
+    press.href = '../../public/Resume_Mackenzie_Joyce.pdf';
+    press.click();
+  };
 
   return (
     <div className="contentLittle">
-      <h3 className="title" style={styles.short}>
+      <h3 className="title" style={styles.underlineSmaller}>
         Resume
       </h3>
-      <h4 style={styles.miniTitle}>Proficiencies</h4>
-      <ul style={styles.sides}>
+      <h4 style={styles.subTitle}>Proficiencies</h4>
+      <ul style={styles.leftAndRight}>
         <div>
           <li>HTML5</li>
           <li>CSS</li>
@@ -47,8 +44,10 @@ const Resume = () => {
           <li>MongoDB</li>
         </div>
       </ul>
-      <h4 style={styles.miniTitle}>Download my Full Resume Here</h4>
-      <button className="btn" title="Resume" onClick={onDownload}>Download</button>
+      <h4 style={styles.subTitle}>Download my Full Resume Here</h4>
+      <button className="btn" title="Resume" onClick={onDownload}>
+        Download
+      </button>
     </div>
   );
 };
