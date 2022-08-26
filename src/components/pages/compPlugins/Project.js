539322d60ca6a42e5fee4335c9497732492ecbb6
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-26 12:47:13 -0400

    WORKS - put in links to each github project and have it opening in new tab

--- src/components/pages/compPlugins/Project.js
+++ src/components/pages/compPlugins/Project.js
@@ -1,19 +1,24 @@
 import React from 'react';
 
-const styles = { 
-  aTag: { 
+const styles = {
+  aTag: {
     padding: 0
-  }, 
-  img: { 
-    height: 440, 
+  },
+  img: {
+    height: 440,
     background: 'rgb(255, 223, 93, 0.5)'
   }
-}
+};
 
 const Project = ({ project }) => {
   return (
     <>
-      <a href={project.link} style={styles.aTag}>
+      <a
+        href={project.link}
+        style={styles.aTag}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <img src={project.img} alt={project.title} style={styles.img} />
         <h3>{project.title}</h3>
       </a>
--- src/components/pages/compPlugins/projects.js
+++ src/components/pages/compPlugins/projects.js
@@ -4,42 +4,48 @@ const projects = [
     img: require('../../../img/backyardJungle.png'),
     title: 'Backyard Jungle',
     text: 'An online forum for Gardeners',
-    tech: 'Handlebars/Bootstrap/Express/RESTful API'
+    tech: 'Handlebars/Bootstrap/Express/RESTful API',
+    link: 'https://github.com/Anastasia095/Backyard-Jungle'
   },
   {
     id: 2,
     img: require('../../../img/mixUpMovieNight.png'),
     title: 'Mix Up Movie Night',
     text: 'Your great night chosen by our app',
-    tech: 'CSS/Bulma/JavaScript'
+    tech: 'CSS/Bulma/JavaScript',
+    link: 'https://github.com/Servbt/Mix-up-Movie-Night'
   },
   {
     id: 3,
     img: require('../../../img/noteTaker.png'),
     title: 'Note Tracker',
     text: 'Simple design for all your complicated tasks',
-    tech: 'React'
+    tech: 'React',
+    link: 'https://github.com/MackenzieJoyce/Note-Taker-React'
   },
   {
     id: 4,
     img: require('../../../img/nerdWall.png'),
     title: 'Conversation Ender',
     text: "Your choices matter, don't let the conversation die",
-    tech: 'CSS/JavaScript'
+    tech: 'CSS/JavaScript',
+    link: 'https://github.com/MackenzieJoyce/Conversation-Ender'
   },
   {
     id: 5,
     img: require('../../../img/techBlog.png'),
     title: 'Tech Blog',
     text: 'Make an account, share thoughts, interact with others',
-    tech: 'Express'
+    tech: 'Express',
+    link: 'https://github.com/MackenzieJoyce/tech-blog'
   },
   {
     id: 6,
     img: require('../../../img/textEditor.png'),
     title: 'Text Editor Extreme',
     text: 'Install the app and take your notes on the go',
-    tech: 'JavaScript'
+    tech: 'JavaScript',
+    link: 'https://github.com/MackenzieJoyce/Text-Editor-Extreme'
   }
 ];
 
