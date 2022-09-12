ea76ec110d49362fb9b9fe11feb5e45c1e4bb31b
Mackenzie <mackenziejoyce414@gmail.com>
2022-09-12 15:52:37 -0400

    added links to portfolio

--- src/pages/components/projects.js
+++ src/pages/components/projects.js
@@ -4,49 +4,56 @@ const projects = [
     img: require('../../assets/img/lookingForCoders.png'),
     title: 'Looking For Coders',
     text: 'Collaborate with other coders and bring your project to life',
-    tech: 'React/Material UI/Nodejs/Express/GraphQL/MongoDB/GitHub API'
+    tech: 'React/Material UI/Nodejs/Express/GraphQL/MongoDB/GitHub API', 
+    link: 'https://github.com/LorenzoFTSIC/LFCoders'
   },
   {
     id: 2,
     img: require('../../assets/img/backyardJungle.png'),
     title: 'Backyard Jungle',
     text: 'An online forum for Gardeners',
-    tech: 'Handlebars/Bootstrap/Nodejs/Express/RESTful API'
+    tech: 'Handlebars/Bootstrap/Nodejs/Express/RESTful API',
+    link: 'https://github.com/Anastasia095/Backyard-Jungle'
   },
   {
     id: 3,
     img: require('../../assets/img/mixUpMovieNight.png'),
     title: 'Mix Up Movie Night',
     text: 'Your great night chosen by our app',
-    tech: 'CSS/Bulma/JavaScript'
+    tech: 'CSS/Bulma/JavaScript',
+    link: 'https://github.com/Servbt/Mix-up-Movie-Night'
   },
   {
     id: 4,
     img: require('../../assets/img/noteTaker.png'),
     title: 'Note Tracker',
     text: 'Simple design for all your complicated tasks',
-    tech: 'React'
+    tech: 'React',
+    link: 'https://github.com/MackenzieJoyce/Note-Taker-React'
   },
   {
     id: 5,
     img: require('../../assets/img/nerdWall.png'),
     title: 'Conversation Ender',
     text: "Your choices matter, don't let the conversation die",
-    tech: 'CSS/JavaScript'
+    tech: 'CSS/JavaScript',
+    link: 'https://github.com/MackenzieJoyce/Conversation-Ender'
   },
   {
     id: 6,
     img: require('../../assets/img/techBlog.png'),
     title: 'Tech Blog',
     text: 'Make an account, share thoughts, interact with others',
-    tech: 'Express'
+    tech: 'Express',
+    link: 'https://github.com/MackenzieJoyce/tech-blog'
   },
   {
     id: 7,
     img: require('../../assets/img/textEditor.png'),
     title: 'Text Editor Extreme',
     text: 'Install the app and take your notes on the go',
-    tech: 'JavaScript'
+    tech: 'JavaScript',
+    link: 'https://github.com/MackenzieJoyce/Text-Editor-Extreme'
   }
 ];
 
