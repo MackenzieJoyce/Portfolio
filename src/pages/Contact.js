4f819e9e1f839144c30085e56a5c60d86ea34152
Mackenzie <mackenziejoyce414@gmail.com>
2022-09-24 17:13:46 -0400

    deleted the contentLittle css rule. deleted the contact list

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -42,83 +42,13 @@ const styles = {
 
 const Contact = () => {
   return (
-    <div className="contentLittle">
-        <h3 className="title contactTitle" style={styles.title}>
-          Get In Touch
-        </h3>
-        <div className="contactList squareContent" style={styles.list}>
-          <div className="contactEmailMe" style={styles.emailMe}>
-            {/* <Link to="/contact/email"> */}
-            <a
-              href="mailto:MackenzieJoyce414@gmail.com"
-              target="_blank"
-              rel="noreferrer"
-              title="Email Address: MackenzieJoyce414@gmail.com"
-            >
-              Email Me!
-            </a>
-            {/* </Link> */}
-          </div>
-          <ul className="contactUlTag" style={styles.ul}>
-            <div>
-              {/* ^ style={styles.sides} */}
-              <li className="contactATag" style={styles.a}>
-                <a
-                  href="https://github.com/MackenzieJoyce"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  title="Github: MackenzieJoyce"
-                >
-                  <span className="contactSpanTag" style={styles.span}>
-                    <FaGithub className="contactIcons" style={styles.icons} />
-                    GitHub - MackenzieJoyce
-                  </span>
-                </a>
-              </li>
-
-              <li className="contactATag" style={styles.a}>
-                <a
-                  href="https://www.linkedin.com/in/mackenziejoyce/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  title="Github: MackenzieJoyce"
-                >
-                  <span className="contactSpanTag" style={styles.span}>
-                    <FaLinkedin className="contactIcons" style={styles.icons} />
-                    LinkedIn - MackenzieJoyce
-                  </span>
-                </a>
-              </li>
-            </div>
-
-            <div>
-              {/* ^ style={styles.sides} */}
-              <li className="contactATag" style={styles.a}>
-                <a
-                  href="https://twitter.com/FreshCodeFish"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  title="Twitter: FreshCodeFish"
-                >
-                  <span className="contactSpanTag" style={styles.span}>
-                    <FaTwitterSquare
-                      className="contactIcons"
-                      style={styles.icons}
-                    />
-                    Twitter - FreshCodeFish
-                  </span>
-                </a>
-              </li>
-
-              <li className="contactATag" style={styles.a}>
-                <span className="contactSpanTag" style={styles.span}>
-                  <FaDiscord className="contactIcons" style={styles.icons} />
-                  Discord - MackenzieJoyce#5843
-                </span>
-              </li>
-            </div>
-          </ul>
-        </div>
+    <div className="contentEntire">
+      <h3 className="title contactTitle" style={styles.title}>
+        Get In Touch
+      </h3>
+      <div>
+        <form></form>
+      </div>
     </div>
   );
 };
