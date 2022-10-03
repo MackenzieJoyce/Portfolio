badfa6e228268e2a25d262436dcd97edd0992f16
Mackenzie <mackenziejoyce414@gmail.com>
2022-10-03 16:28:55 -0400

    added bootstrap to copy pasted email form and styled the form container within the component

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,5 +1,7 @@
 import React, { useRef } from 'react';
-import emailjs from "@emailjs/browser";
+import emailjs from '@emailjs/browser';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const ContactMe = () => {
   const form = useRef();
@@ -25,18 +27,43 @@ const ContactMe = () => {
   };
 
   return (
-    <>
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-      </form>
-    </>
+    <main className="contentPages">
+      <h2>Contact Me</h2>
+      <div className="content">
+        <Form style={styles.formContainer} ref={form} onSubmit={sendEmail}>
+            <Form.Group controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" name="user_name" />
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" name="user_email" />
+            </Form.Group>
+            <Form.Group controlId="formBasicMessage">
+              <Form.Label>Message</Form.Label>
+              <Form.Control as="textArea" name="message" />
+            </Form.Group>
+            <Button type="submit" value="Send">
+              Send
+            </Button>
+          </Form>
+        </div>
+    </main>
   );
 };
+
+const styles = {
+  formContainer: {
+    minWidth: '500px',
+    minHeight: '400px',
+    padding: '5%',
+    background: '#181818',
+    border: 'none',
+    borderRadius: '5px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  }
+}
 
 export default ContactMe;
